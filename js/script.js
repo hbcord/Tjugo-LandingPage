de873cee67c0c1a9f20161fa2ce53611a4741541
Heber Cordova <hbcordova10@gmail.com>
2021-07-12 14:39:01 -0500

    Version final de Tjugo para el curso de IHC

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+/** CODIGO AJAX - NECESITA DE APACHE PARA FUNCIONAR */
+/** GIT HUB NO LO CARGA CORRECTAMENTE */
+
+/** NAV-BAR-TOP */
+
+/*$.get("_navbar_top.html", function(data) {
+    $("#navbar-top").load(data);
+    $("#navbar-top").replaceWith(data);
+
+    $('.menu-toggle').click(function () {
+        $('.navegacion-principal').toggleClass('no-show')
+
+        if ($('.navegacion-principal').hasClass('show')) {
+            $('.navegacion-principal').removeClass('show');
+            $('.navegacion-principal').addClass('no-show');
+        }
+        else {
+            $('.navegacion-principal').removeClass('no-show');
+            $('.navegacion-principal').addClass('show');
+        }
+
+    })
+});*/
+
+/** NAV-BAR-BOTTOM */
+
+/*
+$.get("_navbar_bottom.html", function(data) {
+    $("#navbar-bottom").replaceWith(data);
+});*/
